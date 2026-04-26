3cc305e769ac2d9632784b15a5049c62dc681894
Sentiago <arseny317@gmail.com>
2026-04-26 14:05:39 +0300

    W

--- nixm-backend-webhook/src/main.rs
+++ nixm-backend-webhook/src/main.rs
@@ -335,7 +335,7 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "nixm_webhook=info".into()),
+                .unwrap_or_else(|_| "trace".into()),
         )
         .init();
 
