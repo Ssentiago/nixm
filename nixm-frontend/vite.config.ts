5a40902f24a97fd3b0b60adefe726db25b86d6a2
Sentiago <arseny317@gmail.com>
2026-04-29 15:39:36 +0300

    feat: update vite config
    - Include TypeScript checker and logger context injector
    - Define __APP_VERSION__ env
    - Replace React with Preact to reduce bundle size

--- nixm-frontend/vite.config.ts
+++ nixm-frontend/vite.config.ts
@@ -2,11 +2,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import { readFileSync } from 'node:fs';
+import addLoggerContext from './vite-plugins/add-logger-context';
+
+import checker from 'vite-plugin-checker';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+    tailwindcss(),
+    addLoggerContext(),
+  ],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   resolve: {
     alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+      'react/jsx-runtime': 'preact/jsx-runtime',
       '@': path.resolve(__dirname, './src'),
     },
   },
